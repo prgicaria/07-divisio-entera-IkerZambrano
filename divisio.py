74bc52f0762ae0ee0833f1f531255ac21d503d75
izambrano <izambrano@instituticaria.cat>
2024-10-17 16:54:12 +0200

    Enviament

---
+++ divisio.py
@@ -0,0 +1,7 @@
+D = int(input("INTRODUEIX UN NUMERO "))
+d = int( input("INTRODUEIX UN ALTRE NUMERO "))
+Q = D//d
+r = D % d
+print(f"Divisió: {D}/{d}")
+print(f"Quocient: {Q}")
+print(f"Residu: {r}")
